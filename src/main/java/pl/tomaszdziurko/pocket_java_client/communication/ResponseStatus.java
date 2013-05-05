c4e432e3678c59fb673f644679540cd32cf612ec
Tomasz Dziurko <tdziurko@gmail.com>
2013-05-05 18:46:20 +0200

    Even more statuses

--- src/main/java/pl/tomaszdziurko/pocket_java_client/communication/ResponseStatus.java
+++ src/main/java/pl/tomaszdziurko/pocket_java_client/communication/ResponseStatus.java
@@ -2,7 +2,9 @@ package pl.tomaszdziurko.pocket_java_client.communication;
 
 public enum ResponseStatus {
 
-    OK(0),
+    OK(1),
+    INVALID_REQUEST(130),
+    CONSUMER_KEY_DOES_NOT_MATCH_ACCESS_CODE(137),
     MISSING_CONSUMER_KEY(138),
     MISSING_REDIRECT_URL(140),
     INVALID_CONSUMER_KEY(152),
@@ -12,6 +14,7 @@ public enum ResponseStatus {
     MISSING_CODE(182),
     CODE_NOT_FOUND(185),
     SERVER_ERROR(199),
+
     UNKNOWN_ERROR(-9999);
 
     private final int code;
