d21ab8a422a4d4dd4c37f8156527e9950d0be384
Tomasz Dziurko <tdziurko@gmail.com>
2013-06-02 22:27:07 +0200

    Some example links to use in integration tests

---
+++ src/test/java/pl/tomaszdziurko/pocket_java_client/integration/ExampleLinks.java
@@ -0,0 +1,13 @@
+package pl.tomaszdziurko.pocket_java_client.integration;
+
+public class ExampleLinks {
+
+    public static String GEB_SCREENSHOT_ON_FAIL = "http://tomaszdziurko.pl/2013/05/taking-screenshot-in-failing-ui-tests-using-geb/";
+    public static String GEB_REUSING_BROWSER = "http://tomaszdziurko.pl/2013/05/reusing-browser-instance-geb-ui-tests/";
+    public static String SCALATRA_ESCAPING = "http://tomaszdziurko.pl/2013/04/scalatra-html-js-json-escaping/";
+    public static String XSTREAM = "http://tomaszdziurko.pl/2013/04/xstream-xstreamely-easy-work-xml-data-java/";
+    public static String JASMINE_TEAMCITY = "http://tomaszdziurko.pl/2013/03/jasmine-tests-reporter-teamcity-scala-sbt/";
+    public static String ANGULARJS_STARTER = "http://tomaszdziurko.pl/2013/03/instant-angularjs-starter-book-review/";
+
+
+}
